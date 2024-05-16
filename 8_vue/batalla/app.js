1952ca6a9bd5c6ae0b6aceef9fa0267066abd7a8
David Chaves Rodríguez <A22DavidCR@iessanclemente.net>
2024-05-16 10:25:30 +0200

    vueBatalla

--- 8_vue/batalla/app.js
+++ 8_vue/batalla/app.js
@@ -3,6 +3,12 @@ const app = Vue.createApp({
     return {
       vidaMonstro: 100,
       vidaXogador: 100,
+      contadorEspecial: 0,
+      disabled: true,
+      isPlaying: true,
+      victoria: false,
+      derrota: false,
+      empate: false,
     };
   },
   methods: {
@@ -18,7 +24,59 @@ const app = Vue.createApp({
       alert(`Tes ${this.vidaMonstro} puntos de vida`);
     },
     ataque() {
-      this.vidaMonstro -= this.randomNumber(8, 15);
+      this.vidaMonstro -= this.randomNumber(5, 12);
+      this.finalTurno();
+    },
+    especial() {
+      this.vidaMonstro -= this.randomNumber(10, 25);
+      this.finalTurno();
+    },
+    curacion() {
+      this.vidaXogador += this.randomNumber(8, 20);
+      this.finalTurno();
+    },
+    finalTurno() {
+      this.vidaXogador -= this.randomNumber(8, 15);
+      this.contadorEspecial += 1;
+      if (this.contadorEspecial == 3) {
+        this.disabled = false;
+      }
+      if (this.contadorEspecial == 4) {
+        this.disabled = true;
+        this.contadorEspecial = 0;
+      }
+      if (this.vidaXogador > 100) {
+        this.vidaXogador = 100;
+      }
+      // victoria
+      if (this.vidaMonstro < 0 && this.vidaXogador > 0) {
+        this.vidaMonstro = 0;
+        this.isPlaying = false;
+        this.victoria = true;
+      }
+      // derrota
+      if (this.vidaXogador < 0 && this.vidaMonstro > 0) {
+        this.vidaXogador = 0;
+        this.isPlaying = false;
+        this.derrota = true;
+      }
+      // empate
+      if (this.vidaXogador < 0 && this.vidaMonstro < 0) {
+        this.vidaXogador = 0;
+        this.vidaMonstro = 0;
+        this.isPlaying = false;
+        this.empate = true;
+      }
+    },
+    xogoNovo() {
+      this.vidaMonstro = 100;
+      this.vidaXogador = 100;
+      this.contadorEspecial = 0;
+      this.disabled = true;
+      this.isPlaying = true;
+      this.victoria = false;
+      this.derrota = false;
+      this.empate = false;
     },
   },
 });
